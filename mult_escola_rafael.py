9d4f26bc3389907858ec7cfbbe2496c8279f94a7
rafael <rafael.cds.monteiro@hotmail.com>
2020-05-14 14:50:25 -0300

    Adding logic_of_multipling function.

--- mult_escola_rafael.py
+++ mult_escola_rafael.py
@@ -1,13 +1,31 @@
 import unittest
 
 
-def multiplica(multiplicador, multiplicando):
-    def multiplication(multiplicador, multiplicando):
-        answer = ''
-        for x in reversed(range(0, len(multiplicador))):
-            result = int(multiplicador[x:x + 1]) * int(multiplicando[:1])
+def multiplication(multiplicador, multiplicando):
+    mm = multiplicando[::-1]
+    for x in mm:
+        print(logic_of_multipling(multiplicador, x))
+
+
+def logic_of_multipling(multiplicador, multiplicando):
+    answer = ''
+    value = ''
+    sum_value = 0
+    for x in reversed(range(0, len(multiplicador))):
+        try:
+            sum_value = int(value)
+        except:
+            sum_value = 0
+
+        result = (int(multiplicador[x:x + 1]) * int(multiplicando)) + sum_value
+        if result >= 10:
+            value = str(result)[0:1]
+            answer = answer + str(result)[1:2]
+        else:
+            value = ''
             answer = answer + str(result)
-            print(answer)
+    answer = answer + value
+    return answer[:: -1]
 
 
 class Test_multiplication(unittest.TestCase):
